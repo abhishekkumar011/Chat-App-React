1a54c8e8f0d3b045eff493f1551a3aaa53d84eaf
Abhishek <abhishekkumar0336165@gmail.com>
2023-03-18 20:47:04 +0530

    style: Basic Stucture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react";
+
 function App() {
     return (
         <div>
-            App
+            <Box bg={'blackAlpha.900'}>
+                <Container h={'100vh'} w={'sm'} bg={'white'}>
+                    <VStack h={'full'} py={3}>
+                        <Button fontSize={'sm'} colorScheme={'red'} w={'full'}>
+                            Logout
+                        </Button>
+
+                        <VStack h={'full'} w={'full'} ></VStack>
+
+                        <form style={{width: "100%"}}> 
+                            <HStack> 
+                                <Input placeholder={'Enter a Message...'}/>
+                                <Button fontSize={'sm'} colorScheme={"purple"} type={'submit'}>Send</Button>
+                            </HStack>
+                        </form>
+                    </VStack>
+                </Container>
+            </Box>
         </div>
     );
 }
