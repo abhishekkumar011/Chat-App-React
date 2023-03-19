edb0fa9a468ef406429028d48e3e8d994e06fdc3
Abhishek <abhishekkumar0336165@gmail.com>
2023-03-19 08:23:13 +0530

    feat: sign In added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react";
 import Message from "./components/Message";
+import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { app } from './firebase'
+import { useEffect, useState } from "react";
+
+const auth = getAuth(app);
+
+const loginHandler = () => {
+    const provider = new GoogleAuthProvider()
+
+    signInWithPopup(auth, provider)
+}
 
 function App() {
+
+    const [user, setUser] = useState(false)
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (data) => {
+            setUser(data);
+        })
+    }, [])
+    
+
     return (
         <div>
             <Box bg={'blackAlpha.900'}>
-                <Container h={'100vh'} w={'sm'} bg={'white'}>
-                    <VStack h={'full'} py={3}>
-                        <Button size={'sm'} fontSize={'sm'} colorScheme={'red'} w={'full'}>
-                            Logout
-                        </Button>
+                {
+                    user ? (
+                        <Container h={'100vh'} w={'sm'} bg={'white'}>
+                            <VStack h={'full'} py={3}>
+                                <Button size={'sm'} fontSize={'sm'} colorScheme={'red'} w={'full'}>
+                                    Logout
+                                </Button>
 
-                        <VStack h={'full'} w={'full'} overflowY="auto">
-                            <Message text={'sample message'} />
-                            <Message user="me" text={'sample message'} />
+                                <VStack h={'full'} w={'full'} overflowY="auto">
+                                    <Message text={'sample message'} />
+                                    <Message user="me" text={'sample message'} />
+                                </VStack>
+
+                                <form style={{ width: "100%" }}>
+                                    <HStack>
+                                        <Input size={'sm'} placeholder={'Enter a Message...'} />
+                                        <Button size={'sm'} fontSize={'sm'} colorScheme={"purple"} type={'submit'}>Send</Button>
+                                    </HStack>
+                                </form>
+                            </VStack>
+                        </Container>
+                    ) :
+                        <VStack h={'100vh'} justifyContent={'center'}>
+                            <Button onClick={loginHandler} size={'sm'} fontSize={'sm'} fontWeight={'semibold'} colorScheme={'blue'}>Sign in With Google</Button>
                         </VStack>
-
-                        <form style={{ width: "100%" }}>
-                            <HStack>
-                                <Input size={'sm'} placeholder={'Enter a Message...'} />
-                                <Button size={'sm'} fontSize={'sm'} colorScheme={"purple"} type={'submit'}>Send</Button>
-                            </HStack>
-                        </form>
-                    </VStack>
-                </Container>
+                }
             </Box>
         </div>
     );
