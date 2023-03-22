ba752214279645827c077d4f5b5cb3d0143dbd4e
Abhishek <abhishekkumar0336165@gmail.com>
2023-03-22 08:22:17 +0530

    feat: real-time-messages-implement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { useEffect, useRef, useState } from "react";
 import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react";
 import Message from "./components/Message";
-import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import { app } from './firebase'
-import { useEffect, useState } from "react";
+import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getFirestore, addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 const loginHandler = () => {
     const provider = new GoogleAuthProvider()
-
     signInWithPopup(auth, provider)
 }
 
@@ -16,20 +17,50 @@ const logoutHandler = () => {
     signOut(auth);
 }
 
-function App() {
 
-    const [user, setUser] = useState(false)
+
+function App() {
+    const [user, setUser] = useState(false);
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const divForScroll = useRef(null);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            setMessage('')
+            await addDoc(collection(db, "Messages"), {
+                text: message,
+                uid: user.uid,
+                uri: user.photoURL,
+                createdAt: serverTimestamp()
+            })
+            divForScroll.current.scrollIntoView({ behavior: "smooth" })
+        } catch (error) {
+            alert(error)
+        }
+    }
 
     useEffect(() => {
+        const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
         const unsubscribe = onAuthStateChanged(auth, (data) => {
             setUser(data);
         })
 
+        const unsubscribeForMessage = onSnapshot(q, (snap) => {
+            setMessages(snap.docs.map((item) => {
+                const id = item.id;
+                return { id, ...item.data() };
+            }))
+        })
+
         return () => {
             unsubscribe()
+            unsubscribeForMessage()
         };
     }, []);
-    
+
 
     return (
         <div>
@@ -42,14 +73,18 @@ function App() {
                                     Logout
                                 </Button>
 
-                                <VStack h={'full'} w={'full'} overflowY="auto">
-                                    <Message text={'sample message'} />
-                                    <Message user="me" text={'sample message'} />
+                                <VStack h={'full'} w={'full'} overflowY="auto" css={{"&::-webkit-scrollbar":{
+                                    display: 'none'
+                                }}}>
+                                    {messages.map((item) => (
+                                        <Message key={item.id} user={item.uid === user.uid ? "me" : "other"} text={item.text} uri={item.uri} />
+                                    ))}
+                                    <div ref={divForScroll}></div>
                                 </VStack>
 
-                                <form style={{ width: "100%" }}>
+                                <form onSubmit={submitHandler} style={{ width: "100%" }}>
                                     <HStack>
-                                        <Input size={'sm'} placeholder={'Enter a Message...'} />
+                                        <Input value={message} onChange={(e) => setMessage(e.target.value)} size={'sm'} placeholder={'Enter a Message...'} />
                                         <Button size={'sm'} fontSize={'sm'} colorScheme={"purple"} type={'submit'}>Send</Button>
                                     </HStack>
                                 </form>
